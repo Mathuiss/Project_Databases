aae98a334cced0280a932b63ff8ae5c924331a63
Unknown <mathijsgeerlings@gmail.com>
2018-03-12 10:46:31 +0100

    Share logica dll

---
+++ Someren/Data/KassaUploader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Model;
+
+namespace Data
+{
+    public class KassaUploader
+    {
+        private int GetAantal(int id)
+        {
+            using (SqlConnection connection = Utils.OpenConnectieDB())
+            {
+                string query = "select aantal from VOORRAAD where id = ";
+                query += id;
+                var command = new SqlCommand(query, connection);
+                return (int)command.ExecuteScalar();
+            }
+        }
+
+        public void Betaal(List<VoorraadObject> voorraad)
+        {
+            using (SqlConnection connection = Utils.OpenConnectieDB())
+            {
+                foreach (VoorraadObject item in voorraad)
+                {
+                    int oudAantal = GetAantal(item.Id);
+                    int nieuwAantal = oudAantal - item.Aantal;
+
+                    string query = "update VOORRAAD set aantal = ";
+                    query += nieuwAantal;
+                    query += " where id = ";
+                    query += item.Id;
+
+                    var command = new SqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    
+
+                }
+            }
+        }
+    }
+}
