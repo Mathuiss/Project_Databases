e5b761cfab3dbfa43ba5d3bacdd9931f206224b8
Unknown <sander.de.veth@gmail.com>
2018-03-27 12:14:28 +0200

    Auto stash before merge of "Share" and "origin/Share"

--- SpartaDAL/SpartaDAL/DALOverzicht.cs
+++ SpartaDAL/SpartaDAL/DALOverzicht.cs
@@ -11,8 +11,27 @@ namespace Sparta.Dal
         {
             List<Cursus> cursussen = new List<Cursus>();
 
-            
+            using (SqlConnection connection = DALConnection.GetConnectionByName("Database"))
+            {
+                SqlCommand command = new SqlCommand("" +
+                    "select CursusId, Naam, Niveau, Toelichting, Categorie from Cursus", connection);
+                connection.Open();
 
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Cursus cursus = new Cursus();
+                        cursus.Id = reader.GetInt32(0);
+                        cursus.Naam = reader.GetString(1);
+                        cursus.Niveau = reader.GetInt32(2);
+                        cursus.Toelichting = reader.GetString(3);
+                        cursus.Categorie = (DeelnemerCategorie)reader.GetInt32(4);
+                    }
+                }
+            }
             return cursussen;
         }
 
