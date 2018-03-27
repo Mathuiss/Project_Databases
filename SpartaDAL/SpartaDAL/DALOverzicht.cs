676df52cdf7e802f60050ae98b17f71c09e35827
Unknown <sander.de.veth@gmail.com>
2018-03-27 11:18:40 +0200

    Auto stash before merge of "Share" and "origin/Share"

---
+++ SpartaDAL/SpartaDAL/DALOverzicht.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sparta.Model;
+
+namespace Sparta.Dal
+{
+    public static class DALOverzicht
+    {
+        public static List<Cursus> GetCursussen()
+        {
+            List<Cursus> cursussen = new List<Cursus>();
+
+            
+
+            return cursussen;
+        }
+    }
+}
