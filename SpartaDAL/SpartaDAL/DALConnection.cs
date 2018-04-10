0b15ee3b004846d1b6f922eb1db61e57ddc58743
Mathuis G <mathijsgeerlings@gmail.com>
2018-04-10 10:42:14 +0200

    Update

--- SpartaDAL/SpartaDAL/DALConnection.cs
+++ SpartaDAL/SpartaDAL/DALConnection.cs
@@ -9,6 +9,8 @@ namespace Sparta.Dal
         public static SqlConnection GetConnectionByName(string name)
         {
             string connectionString = "Data Source=den1.mssql4.gear.host;Initial Catalog=projectdbgroepa1;Persist Security Info=True;User ID=projectdbgroepa1;Password=Zc2pQwg-wK_w";
+            //string connectionString = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
 
             var connection = new SqlConnection(connectionString);
             return connection;
--- SpartaDAL/UnitTest/Program.cs
+++ SpartaDAL/UnitTest/Program.cs
@@ -8,47 +8,13 @@ using System.Drawing;
 namespace UnitTest
 {
     class Program
-    {
-        private static DateTimePicker dtp;
-
-        [STAThread]
+    {   
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    Contact contact = new Contact();
-            //    // contact.Id wordt door de database aangemaakt
-            //    contact.Persoonid = 2;
-            //    contact.Straat = "Zijweg";
-            //    contact.Huisnummer = 3;
-            //    contact.Huisnummertoevoeging = "zw";
-            //    contact.Plaats = "Haarlem";
-            //    contact.Postcode = "1234AB";
-            //    contact.Email = "daaro@weetikwel.nl";
-            //    contact.Telefoon = "0654321678";
-                
-            //    DALGebruiker.voegtoeContactInfo(contact);
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.ToString());
-            //}
-
             try
             {
-                Contact contact = new Contact();
-                // contact.Id wordt door de database aangemaakt
-                contact.Id = 15;
-                contact.Persoonid = 2;
-                contact.Straat = "Amsterdamstraat";
-                contact.Huisnummer = 10;
-                contact.Huisnummertoevoeging = "A";
-                contact.Plaats = "Haarlem";
-                contact.Postcode = "1234AB";
-                contact.Email = "daaro@weetikwel.nl";
-                contact.Telefoon = "0678912345";
-
-                DALGebruiker.vernieuwContactInfo(contact);
+                Contact c = DALGebruiker.GetContactInfoByPersoonId(26);
+                Console.WriteLine("{0} {1}", c.Id, c.Email);
             }
             catch (Exception ex)
             {
@@ -56,17 +22,6 @@ namespace UnitTest
             }
 
             Console.ReadKey();
-        }
-
-        private static void Btn_Click(object sender, EventArgs e)
-        {
-            DALGebruiker.RegistreerPersoon((DeelnemerCategorie)100, "mathijs", "geerlings", dtp.Value, new Login("m", "m"));
-            List<Persoon> personen = DALOverzicht.GetPersonen();
-
-            foreach (Persoon p in personen)
-            {
-                Console.WriteLine(p.Persoonid + " " + p.Naam);
-            }
         }
     }
 }
